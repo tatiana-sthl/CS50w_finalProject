39d18e1628bf45427a141828bd717db6793d6399
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-04-05 11:04:03 +0200

    add sign up

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,21 @@
+from django.conf import settings
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
+from django.views.generic import View
 
 from . import forms
 
-def login_page(request):
-    form = forms.LoginForm()
-    message = ''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+        
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             user = authenticate(
                 username=form.cleaned_data['username'],
@@ -15,10 +23,20 @@ def login_page(request):
             )
             if user is not None:
                 login(request, user)
-                return redirect('home')                
+                return redirect('home')
         message = 'Invalid identifiers.'
-    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
+        return render(request, self.template_name, context={'form': form, 'message': message})
     
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
