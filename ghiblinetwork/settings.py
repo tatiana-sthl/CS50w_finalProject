f2f25da005c4b9e4797d3953c77f0bb1c346d8a5
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-04-05 10:52:39 +0200

    add login and logout

--- ghiblinetwork/settings.py
+++ ghiblinetwork/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ghiblinetwork.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User'
+
+LOGIN_URL = 'login'
