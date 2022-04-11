0c3fb05ce130020faa06588e6eb2482b3a3204cd
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-04-11 10:57:53 +0200

    change details and css

--- ghiblinetwork/settings.py
+++ ghiblinetwork/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+import os.path
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
